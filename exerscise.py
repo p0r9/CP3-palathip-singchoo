ae82a82a7637d014fffa15a6dcf7172f562bea34
p0r9 <palathip.opor@gmail.com>
2021-03-29 13:16:44 +0700

    exercise4
    output score

---
+++ exerscise.py
@@ -0,0 +1,9 @@
+FE  = 35.5
+GB  = 23.5
+ICS = 27.5
+CP  = 32.5
+print ('--- Your Score ---')
+print ('Foundation English :',FE)
+print ('General Business :',GB)
+print ('Introduction to Computer Systems :',ICS)
+print ('Computer Programming :',CP)
