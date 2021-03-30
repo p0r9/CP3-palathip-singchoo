87da1685d60ade3181ba34c3eb4c91422fd503b3
p0r9 <palathip.opor@gmail.com>
2021-03-30 12:23:01 +0700

    input1
    +-*/

---
+++ exerscise 5_1_Palathip_S.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+print(str(a)+' + '+str(b)+' = '+str(a+b))
+print(str(a)+' - '+str(b)+' = '+str(a-b))
+print(str(a)+' * '+str(b)+' = '+str(a*b))
+print(str(a)+' / '+str(b)+' = '+str(a//b))
