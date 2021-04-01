7dc88405f161727d6c1359f29b2720ad1763c7a2
p0r9 <palathip.opor@gmail.com>
2021-04-01 16:44:16 +0700

    while login

---
+++ exerscise 9_Palathip_S.py
@@ -0,0 +1,8 @@
+Username = input('Username :')
+Password = input('Password :')
+while Username != 'customer15' or Password != '12345678' :
+    print ('user or password wrong !!!')
+    Username = input('Username :')
+    Password = input('Password :')
+print ('Done !! :D')
+print ('wellcome',Username,':D')
