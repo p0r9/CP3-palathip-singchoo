ed659490aaeffa7baeba096b4af5f0b4038b6ff7
p0r9 <81456752+p0r9@users.noreply.github.com>
2021-04-04 11:57:56 +0700

    Update exerscise 11_Palathip_S.py

--- exerscise 11_Palathip_S.py
+++ exerscise 11_Palathip_S.py
@@ -1,6 +1,5 @@
 stair = int(input('stair : '))
 for i in range (stair) :
-    #print(" " *(stair - i -1) + ("*" * (2 * i +1)))
     space = stair
     space -= i-1
-    print ((space*' ')+('*'*(i+1)))
+    print ((space*' ')+('*'*2(i+1)))
