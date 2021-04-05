061ae16a67258028d8d4095e342226f043154ad8
p0r9 <palathip.opor@gmail.com>
2021-04-05 13:50:49 +0700

    funtion login mind shop

---
+++ Lecture 54_Palathip_S.py
@@ -0,0 +1,30 @@
+def login () :
+    usernameInput = input("Username :")
+    passwordInput = input("Password :")
+    if usernameInput  == "customer115"  and passwordInput == "12345678":
+        return showMenu()
+    else :
+        print ('username or password Wrong !!')
+        return login()
+def showMenu () :
+    print('Done !!! :D')
+    print("----- mindShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    return menuSelect ()
+def menuSelect () :
+    userSelected = int(input(">>"))
+    if userSelected == 1 :
+        total_price = int(input('your totalprice : ')) 
+        print (vatcalculate (total_price))
+    else :
+        return pricecalculate ()
+def vatcalculate (totalprice) :
+    vat = 7
+    result = totalprice + (totalprice *7/100)
+    return result
+def pricecalculate () :
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    print ( vatcalculate(price1+price2) )
+login ()
