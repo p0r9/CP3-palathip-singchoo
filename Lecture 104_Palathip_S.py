6660ea4fda24dc0ab51bebce27ac8c03b8a6cd63
p0r9 <palathip.opor@gmail.com>
2021-04-12 09:12:41 +0700

    class

---
+++ Lecture 104_Palathip_S.py
@@ -0,0 +1,22 @@
+class Customer :
+    name = ''
+    lastname = ''
+    age = 0
+
+    def Addcart (self) :
+        print ('Added to',self.name,self.lastname,'cart')
+
+customer1 = Customer()
+customer1.name = 'Thanachat'
+customer1.lastname = 'stang'
+customer1.Addcart()
+
+customer2 = Customer()
+customer2.name = 'Vasavat'
+customer2.lastname = 'Thee-o'
+customer2.Addcart()
+
+customer3 = Customer()
+customer3.name = 'Jirapat'
+customer3.lastname = 'James'
+customer3.Addcart()
